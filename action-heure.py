e87621bfc23f45c2ed64af386f3d9bfb3b9b1ad2
jrimet <johan.rimet@preciamolen.fr>
2019-07-23 09:34:45 +0200

    m

--- action-heure.py
+++ action-heure.py
@@ -1,15 +1,12 @@
-# -*- coding: utf-8 -*-
+#!/usr/bin/env python2
 from hermes_python.hermes import Hermes
 from datetime import datetime
-from datetime import date
 from pytz import timezone
-import ConfigParser
-import io
 
-CONFIGURATION_ENCODING_FORMAT = "utf-8"
-CONFIG_INI = "config.ini"
+MQTT_IP_ADDR = "localhost"
+MQTT_PORT = 1883
+MQTT_ADDR = "{}:{}".format(MQTT_IP_ADDR, str(MQTT_PORT))
 
-MQTT_ADDR = "localhost:1883"
 
 def verbalise_hour(i):
 	if i == 0:
@@ -21,7 +18,7 @@ def verbalise_hour(i):
 	elif i == 21:
 		return "vingt et une heures"
 	else:
-		return "{0} heures".format(str(i))
+		return "{0} heures".format(str(i)) 
 
 def verbalise_minute(i):
 	if i == 0:
@@ -37,47 +34,28 @@ def verbalise_minute(i):
 	elif i == 51:
 		return "cinquante et une"
 	else:
-		return "{0}".format(str(i))
-
-class SnipsConfigParser(ConfigParser.SafeConfigParser):
-    def to_dict(self):
-        return {section : {option_name : option for option_name, option in self.items(section)} for section in self.sections()}
+		return "{0}".format(str(i)) 
 
 
-def read_configuration_file(configuration_file):
-    try:
-        with io.open(configuration_file, encoding=CONFIGURATION_ENCODING_FORMAT) as f:
-            conf_parser = SnipsConfigParser()
-            conf_parser.readfp(f)
-            return conf_parser.to_dict()
-    except (IOError, ConfigParser.Error) as e:
-        return dict()
+def intent_received(hermes, intent_message):
 
-def subscribe_intent_callback(hermes, intentMessage):
-    conf = read_configuration_file(CONFIG_INI)
-    action_wrapper(hermes, intentMessage, conf)
+	print()
+	print(intent_message.intent.intent_name)
+	print ()
 
+	if intent_message.intent.intent_name == 'ustaN:askTime':
 
-def action_wrapper(hermes, intentMessage, conf):
-	print(intentMessage)
-	if intentMessage.intent.intent_name == 'ustaN:heure':
 		sentence = 'Il est '
+		print(intent_message.intent.intent_name)
+
 		now = datetime.now(timezone('Europe/Paris'))
+
 		sentence += verbalise_hour(now.hour) + " " + verbalise_minute(now.minute)
 		print(sentence)
-		hermes.publish_end_session(intentMessage.session_id, sentence)
-	elif intentMessage.intent.intent_name == 'ustaN:date':
-		MonthList = ['Janvier','Fevrier','Mars','Avril','Mai','Juin','Juillet','Aout','Septembre','Octobre','Novembre','Decembre']
-		DayList = ['Lundi','Mardi','Mercredi','Jeudi','Vendredi','Samedi','Dimanche']
-		dayNumber = date.today().day
-		weekday = DayList[date.today().weekday()-1]
-		sentence = "On est le " + weekday + " " + str(dayNumber) + " "+ MonthList[date.today().month-1]
-		print(sentence)
-		hermes.publish_end_session(intentMessage.session_id, sentence)
-	else :
-		hermes.publish_end_session(intentMessage.session_id, "erreur!")
 
-if __name__ == "__main__":
-    with Hermes("localhost:1883") as h:
-        h.subscribe_intent("ustaN:date", subscribe_intent_callback) \
-.start()
+		# hermes.publish_continue_session(intent_message.session_id, sentence, ["Joseph:greetings"])
+		hermes.publish_end_session(intent_message.session_id, sentence)
+
+
+with Hermes(MQTT_ADDR) as h:
+	h.subscribe_intents(intent_received).start()
